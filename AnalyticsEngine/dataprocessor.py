3f4a6ae7c8fd3a8dbe85f70655e52147c83ffd55
iKnock <ergana.henok@gmail.com>
2022-05-19 19:20:16 +0200

    extract massive data

--- AnalyticsEngine/dataprocessor.py
+++ AnalyticsEngine/dataprocessor.py
@@ -5,23 +5,29 @@ Created on Mon May 16 12:55:16 2022
 @author: HSelato
 """
 
-import ccxt
 
-import asyncio
 #import ccxt.async_support as ccxt
 #async def print_poloniex_ethbtc_ticker():
 #    poloniex = ccxt.poloniex()
 #    print(await poloniex.fetch_ticker('ETH/BTC'))
 
+import ccxt
+import asyncio
 import pandas as pd
 from dataminner import *
+
+import sys
+import requests
+import json
+from influx_line_protocol import Metric
+
+import matplotlib.pyplot as plt
+import datetime as dt
+import numpy as np
 
 from indicators import *
 from strategy_performance_index import StrategyPerformanceIndex
 from portofolio_rebalancing import Backtesting
-import matplotlib.pyplot as plt
-import datetime as dt
-import numpy as np
 from breakout import *
 
 from asciichart import *
@@ -134,7 +140,7 @@ def btc_binance_candle(candle_size):
     
     ticker = "BTC/USDT"    
     
-    from_datetime = '2022-01-01 00:00:00'
+    from_datetime = '2021-08-01 18:40:00'
     from_timestamp = exchange.parse8601(from_datetime)    
     now = exchange.milliseconds()
     
@@ -169,14 +175,37 @@ def btc_binance_candle(candle_size):
             time.sleep(hold)
     
     
-    df_1h_candle = pd.DataFrame(candle_data)
-    df_1h_candle.columns  = ["TIMESTAMP", "OPEN", "HIGH", "LOW", "CLOSE", "VOLUME"]
+    df_candle = pd.DataFrame(candle_data)
+    df_candle.columns  = ["TIMESTAMP", "OPEN", "HIGH", "LOW", "CLOSE", "VOLUME"]
 
     
-    df_1h_candle = df_1h_candle.set_index('TIMESTAMP')
-    df_1h_candle['DATE'] = pd.to_datetime(candle_data.index, utc=True, unit='ms').tz_convert('europe/rome')
-    df_1h_candle = df_1h_candle.set_index('DATE')
-    return df_1h_candle
+    df_candle = df_candle.set_index('TIMESTAMP')
+    df_candle['DATE'] = pd.to_datetime(candle_data.index, utc=True, unit='ms').tz_convert('europe/rome')
+    df_candle = df_candle.set_index('DATE')
+        
+    return df_candle
+
+host = 'http://localhost:9000'
+
+def run_query(sql_query):
+    query_params = {'query': sql_query, 'fmt' : 'json'}
+    try:
+        response = requests.get(host + '/exec', params=query_params)
+        json_response = json.loads(response.text)
+        print(json_response)
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}', file=sys.stderr)
+        
+ # create table
+run_query("CREATE TABLE IF NOT EXISTS tbl_btc_5m_candle (insert_ts TIMESTAMP, DATE TIMESTAMP, OPEN DOUBLE, HIGH DOUBLE, LOW DOUBLE, CLOSE DOUBLE, VOLUME DOUBLE ")
+ 
+ # insert row
+run_query("INSERT INTO tbl_btc_5m_candle VALUES(now(), "+123456+")")          
+
+# Current time in nanoseconds
+def current_timestamp():
+    return int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() * 1000) * 1000000
+             
 
 def calc_and_add_indicators(DF):
     #binance_btc_5m_ohlcv = copy.deepcopy(DF)
@@ -200,11 +229,9 @@ def calc_and_add_indicators(DF):
     return binance_btc_5m_ohlcv;
 
 
-btc_binance_5m_candle = btc_binance_candle("1d")
+btc_binance_5m_candle = btc_binance_candle("5m")
 
 btc_bina_5m_candle_with_indicators = pd.DataFrame(calc_and_add_indicators(binance_btc_5m_ohlcv))
-
-df_5m_candle.to_csv("df_5m_candle", sep='\t')
 
 
 print("\n" + plot(df_5m_candle['CLOSE'][-40:],{'height': 15}))  # print the chart
---
+++ DataminingEngine/scrap_candles.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 15:36:25 2022
+
+@author: HSelato
+"""
+
+import os
+from pathlib import Path
+
+import sys
+import csv
+import requests
+import pandas as pd
+
+# -----------------------------------------------------------------------------
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(''))))
+sys.path.append(root + '/codes/TRADER-ENGINE/trader_engine/csv')
+
+import ccxt
+
+
+# -----------------------------------------------------------------------------
+
+def retry_fetch_ohlcv(exchange, max_retries, symbol, timeframe, since, limit):
+    num_retries = 0
+    try:
+        num_retries += 1
+        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since, limit)
+        # print('Fetched', len(ohlcv), symbol, 'candles from', exchange.iso8601 (ohlcv[0][0]), 'to', exchange.iso8601 (ohlcv[-1][0]))
+        return ohlcv
+    except Exception:
+        if num_retries > max_retries:
+            raise  # Exception('Failed to fetch', timeframe, symbol, 'OHLCV in', max_retries, 'attempts')
+
+
+def scrape_ohlcv(exchange, max_retries, symbol, timeframe, since, limit):
+    earliest_timestamp = exchange.milliseconds()
+    timeframe_duration_in_seconds = exchange.parse_timeframe(timeframe)
+    timeframe_duration_in_ms = timeframe_duration_in_seconds * 1000
+    timedelta = limit * timeframe_duration_in_ms
+    all_ohlcv = []
+    while True:
+        fetch_since = earliest_timestamp - timedelta
+        ohlcv = retry_fetch_ohlcv(exchange, max_retries, symbol, timeframe, fetch_since, limit)
+        if len(ohlcv) > 0:
+            # if we have reached the beginning of history
+            print('*******************************************')
+            print(len(ohlcv))
+            print('*******************************************')
+            if ohlcv[0][0] >= earliest_timestamp:
+                break
+            earliest_timestamp = ohlcv[0][0]
+            all_ohlcv = ohlcv + all_ohlcv
+            print(len(all_ohlcv), symbol, 'candles in total from', exchange.iso8601(all_ohlcv[0][0]), 'to', exchange.iso8601(all_ohlcv[-1][0]))
+        # if we have reached the checkpoint
+            if fetch_since < since:
+                break
+        else:
+            print(symbol +' has no market data ')
+            return all_ohlcv
+    return all_ohlcv
+
+def write_to_csv(filename, exchange, data):
+    p = Path("../data/raw/", str(exchange))
+    p.mkdir(parents=True, exist_ok=True)
+    full_path = p / str(filename)
+    
+    data.to_csv(full_path, sep='\t')
+    """
+    with Path(full_path).open('w+', newline='') as output_file:
+        csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerows(data)
+    """
+    return full_path
+
+
+def scrape_candles_to_csv(filename, exchange_id, max_retries, symbol, timeframe, since, limit):
+    # instantiate the exchange by id
+    exchange = getattr(ccxt, exchange_id)({
+        'enableRateLimit': True,  # required by the Manual
+    })
+    # convert since from string to milliseconds integer if needed
+    if isinstance(since, str):
+        since = exchange.parse8601(since)
+    # preload all markets from the exchange
+    exchange.load_markets()
+    # fetch all candles
+    ohlcv = scrape_ohlcv(exchange, max_retries, symbol, timeframe, since, limit)
+    if len(ohlcv) > 0:
+        
+        df_candle = pd.DataFrame(ohlcv)
+        df_candle.columns  = ["TIMESTAMP", "OPEN", "HIGH", "LOW", "CLOSE", "VOLUME"]
+        
+        df_candle = df_candle.set_index('TIMESTAMP')
+        df_candle['DATE'] = pd.to_datetime(df_candle.index, utc=True, unit='ms').tz_convert('europe/rome')
+        df_candle = df_candle.set_index('DATE')
+                
+        # save them to csv file
+        full_path = write_to_csv(filename, exchange, df_candle)
+        
+        import_csv_to_quest_db(full_path)
+
+        print('Saved', len(ohlcv), 'candles from', exchange.iso8601(ohlcv[0][0]), 'to', exchange.iso8601(ohlcv[-1][0]), 'to', filename)
+        return df_candle
+    else:
+        print("No data found for "+symbol+ " at "+exchange_id)
+
+def import_csv_to_quest_db(file_path):
+    print('*******************************************')
+    print(file_path)
+    print('*******************************************')
+    
+    files = {
+        'data': open(file_path, 'rb'),
+    }
+
+    response = requests.post('http://localhost:9000/imp', files=files)
+    
+    print('*******************************************')
+    print(response)
+    print('*******************************************')
+            
+sol_usdt_1m = scrape_candles_to_csv('sol_usdt_1m.csv', 'binance', 3, 'SOL/USDT', '5m', '2021-05-1900:00:00Z', 1000)
